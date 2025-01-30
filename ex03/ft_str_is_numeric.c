050aa243dd773384c46ba0c971e92be3b0357988
FranciscoLi2000 <francisco2000li@icloud.com>
2025-01-30 00:48:45 +0000

    检查字符串是否有数字

---
+++ ex03/ft_str_is_numeric.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] >= 48 && str[i] <= 57)
+		return (1);
+	return (0);
+}
+#include <stdio.h>
+int	main()
+{
+	char	s[100];
+
+	scanf("%99s", s);
+	printf("%i\n", ft_str_is_numeric(s));
+	return (0);
+}
