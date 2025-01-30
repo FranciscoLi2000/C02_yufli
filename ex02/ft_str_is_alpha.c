185e700fd11529cb8d1e2fa80ed933142bf4f541
FranciscoLi2000 <francisco2000li@icloud.com>
2025-01-30 01:16:49 +0000

    ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -4,8 +4,12 @@ int	ft_str_is_alpha(char *str)
 	int	i;
 
 	i = 0;
-	while ((str[i] >= 65 && str[i] <= 91) || (str[i] >= 97 && str[i] <= 122))
-		return (1);
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 65 && str[i] <= 91) || (str[i] >= 97 && str[i] <= 122))
+			return (1);
+		i++;
+	}
 	return (0);
 }
 #include <stdio.h>
--- ex03/ft_str_is_numeric.c
+++ ex03/ft_str_is_numeric.c
@@ -4,8 +4,12 @@ int	ft_str_is_numeric(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] >= 48 && str[i] <= 57)
-		return (1);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+			return (1);
+		i++;
+	}
 	return (0);
 }
 #include <stdio.h>
