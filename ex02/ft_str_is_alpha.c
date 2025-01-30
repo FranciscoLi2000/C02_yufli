32071445ed8e6e231502ea95263d1b041d467a27
FranciscoLi2000 <francisco2000li@icloud.com>
2025-01-30 00:42:49 +0000

    检查字符串是否有字母

---
+++ ex02/ft_str_is_alpha.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while ((str[i] >= 65 && str[i] <= 91) || (str[i] >= 97 && str[i] <= 122))
+		return (1);
+	return (0);
+}
+#include <stdio.h>
+int	main()
+{
+	char	s[100];
+
+	scanf("%99s", s);
+	printf("%i\n", ft_str_is_alpha(s));
+	return (0);
+}
