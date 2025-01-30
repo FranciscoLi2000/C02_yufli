f0958f65d682eb1a65a318ef1a6fc6b58ea181b5
FranciscoLi2000 <francisco2000li@icloud.com>
2025-01-30 01:11:07 +0000

    检查字符串是否有小写字母

---
+++ ex04/ft_str_is_lowercase.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+#include <stdio.h>
+int	main()
+{
+	char	s[100];
+	scanf("%99s", s);
+	printf("%i\n", ft_str_is_lowercase(s));
+	return (0);
+}
